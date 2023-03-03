03c0a8c12c209fa85d263a2ddda1b999ed1e053e
David Kaufmann <david@kaufmann.dev>
2023-03-03 13:54:02 +0200

    fix: update .gitignore

--- Model/Migrations/20230408015133_mig132.Designer.cs
+++ Model/Migrations/20230408015133_mig132.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Model.Configurations;
 
@@ -10,9 +11,10 @@ using Model.Configurations;
 namespace Model.Migrations
 {
     [DbContext(typeof(AuctionDbContext))]
-    partial class AuctionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230408015133_mig132")]
+    partial class mig132
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -90,7 +92,7 @@ namespace Model.Migrations
 
                     b.HasIndex("CustomerId");
 
-                    b.ToTable("AUCTION_HAS_ITEMS_JT");
+                    b.ToTable("AuctionItems");
                 });
 
             modelBuilder.Entity("Model.Entities.Auctions.Customer", b =>
---
+++ Model/Migrations/20230408015914_mig133.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Model.Migrations
+{
+    public partial class mig133 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuctionItems_CUSTOMERS_CUSTOMER_ID",
+                table: "AuctionItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuctionItems_WORKS_OF_ART_BT_ARTWORK_ID",
+                table: "AuctionItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AuctionItems",
+                table: "AuctionItems");
+
+            migrationBuilder.RenameTable(
+                name: "AuctionItems",
+                newName: "AUCTION_HAS_ITEMS_JT");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AuctionItems_CUSTOMER_ID",
+                table: "AUCTION_HAS_ITEMS_JT",
+                newName: "IX_AUCTION_HAS_ITEMS_JT_CUSTOMER_ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AUCTION_HAS_ITEMS_JT",
+                table: "AUCTION_HAS_ITEMS_JT",
+                columns: new[] { "ARTWORK_ID", "CUSTOMER_ID" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AUCTION_HAS_ITEMS_JT_CUSTOMERS_CUSTOMER_ID",
+                table: "AUCTION_HAS_ITEMS_JT",
+                column: "CUSTOMER_ID",
+                principalTable: "CUSTOMERS",
+                principalColumn: "CUSTOMER_ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AUCTION_HAS_ITEMS_JT_WORKS_OF_ART_BT_ARTWORK_ID",
+                table: "AUCTION_HAS_ITEMS_JT",
+                column: "ARTWORK_ID",
+                principalTable: "WORKS_OF_ART_BT",
+                principalColumn: "ARTWORK_ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AUCTION_HAS_ITEMS_JT_CUSTOMERS_CUSTOMER_ID",
+                table: "AUCTION_HAS_ITEMS_JT");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AUCTION_HAS_ITEMS_JT_WORKS_OF_ART_BT_ARTWORK_ID",
+                table: "AUCTION_HAS_ITEMS_JT");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AUCTION_HAS_ITEMS_JT",
+                table: "AUCTION_HAS_ITEMS_JT");
+
+            migrationBuilder.RenameTable(
+                name: "AUCTION_HAS_ITEMS_JT",
+                newName: "AuctionItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AUCTION_HAS_ITEMS_JT_CUSTOMER_ID",
+                table: "AuctionItems",
+                newName: "IX_AuctionItems_CUSTOMER_ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AuctionItems",
+                table: "AuctionItems",
+                columns: new[] { "ARTWORK_ID", "CUSTOMER_ID" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuctionItems_CUSTOMERS_CUSTOMER_ID",
+                table: "AuctionItems",
+                column: "CUSTOMER_ID",
+                principalTable: "CUSTOMERS",
+                principalColumn: "CUSTOMER_ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuctionItems_WORKS_OF_ART_BT_ARTWORK_ID",
+                table: "AuctionItems",
+                column: "ARTWORK_ID",
+                principalTable: "WORKS_OF_ART_BT",
+                principalColumn: "ARTWORK_ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
